820afb948a5eef78d57f1e2d54aab72f84e15960
Juan Martín <jwancortez@gmail.com>
2023-03-07 16:49:31 -0300

    Homework M09 terminada

--- M09 Ejercicios Extra/Ejercicios Extra.js
+++ M09 Ejercicios Extra/Ejercicios Extra.js
@@ -6,7 +6,11 @@ function deObjetoAarray(objeto) {
    // Estos elementos debe ser cada par clave:valor del objeto recibido.
    // [EJEMPLO]: {D: 1, B: 2, C: 3} ---> [['D', 1], ['B', 2], ['C', 3]].
    // Tu código:
+   return Object.entries(objeto)
+   
 }
+
+
 
 function numberOfCharacters(string) {
    // La función recibe un string. Debes recorrerlo y retornar un objeto donde cada propiedad es una de las
@@ -14,7 +18,19 @@ function numberOfCharacters(string) {
    // Las letras deben estar en orden alfabético.
    // [EJEMPLO]: "adsjfdsfsfjsdjfhacabcsbajda" ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 }
    // Tu código:
+   let arr=string.split("")
+   let obj={}
+   for (let i=0; i<arr.length; i++){
+      if (obj.hasOwnProperty(arr[i])) {
+         obj[arr[i]]++
+      }else {
+         obj[arr[i]]=1
+      }
+   }
+   return obj
 }
+
+
 
 function capToFront(string) {
    // Recibes un string con algunas letras en mayúscula y otras en minúscula.
@@ -22,26 +38,61 @@ function capToFront(string) {
    // Retornar el string.
    // [EJEMPLO]: soyHENRY ---> HENRYsoy
    // Tu código:
+   let arr=string.split("")
+   let arrMayusculas=arr.filter((char)=>{
+      return char==char.toUpperCase()
+   })
+   let arrMinusculas=arr.filter((char)=>{
+      return char==char.toLowerCase()
+   })
+   let finalArr=arrMayusculas.concat(arrMinusculas)
+   let finalStr=finalArr.join("")
+   return finalStr
+
 }
+
 
 function asAmirror(frase) {
    // Recibes una frase. Tu tarea es retornar un nuevo string en el que el orden de las palabras sea el mismo.
    // La diferencia es que cada palabra estará escrita al inverso.
    // [EJEMPLO]: "The Henry Challenge is close!"  ---> "ehT yrneH egnellahC si !esolc"
    // Tu código:
+   let arr=frase.split(" ")
+   let arr2=[]
+   for (let i=0; i<arr.length; i++){
+      let subArr=arr[i].split("")
+      let subArrInverse=subArr.reverse()      
+      arr2.push(subArrInverse.join(""))
+   }   
+   return arr2.join(" ")
 }
 
 function capicua(numero) {
    // Si el número que recibes es capicúa debes retornar el string: "Es capicua".
    // Caso contrario: "No es capicua".
    // Tu código:
+   let chequeo=numero.toString()
+   chequeo=chequeo.split("").reverse().join("")
+   if(chequeo==numero){
+      return "Es capicua"
+   }else{
+   return "No es capicua"}
 }
 
 function deleteAbc(string) {
    // Tu tarea es eliminar las letras "a", "b" y "c" del string recibido.
    // Retorna el string sin estas letras.
    // Tu código:
+   let arr=string.split("")
+   let arr2=arr.filter((char)=>{
+      return char!="a"&&char!="b"&&char!="c"
+   })
+
+   return arr2.join("")
+   
 }
+
+
 
 function sortArray(arrayOfStrings) {
    // Recibes un arreglo de strings.
@@ -49,7 +100,11 @@ function sortArray(arrayOfStrings) {
    // de la longitud de cada string.
    // [EJEMPLO]: ["You", "are", "beautiful", "looking"]  ---> [“You", "are", "looking", "beautiful"]
    // Tu código:
+   return arrayOfStrings.sort(function(a,b){
+      return a.length-b.length
+   })
 }
+
 
 function buscoInterseccion(array1, array2) {
    // Recibes dos arreglos de números.
@@ -58,6 +113,10 @@ function buscoInterseccion(array1, array2) {
    // Si no tienen elementos en común, retornar un arreglo vacío.
    // [PISTA]: los arreglos no necesariamente tienen la misma longitud.
    // Tu código:
+   let finalArray=array1.filter((num)=>{
+      return array2.includes(num)
+   })
+   return finalArray
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
